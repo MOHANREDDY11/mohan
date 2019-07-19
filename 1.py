c28ed85447462028c553c552a9f804449513cc59
MOHANREDDY11 <53077856+MOHANREDDY11@users.noreply.github.com>
2019-07-19 15:22:39 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,9 +1,8 @@
 #kmrs1997k@gmail.com
 number= int(input())
-if (number>0):
-   if(number%2=0):
+ if(number%2)==0:
          print("even")
-       elif(number!=0):
+       elif(number%2)!==0:
           print("odd")
 else:
       print("invalid")
