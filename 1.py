0de893096486f2b601f9cfa71461cacbe9c43917
MOHANREDDY11 <53077856+MOHANREDDY11@users.noreply.github.com>
2019-07-19 15:03:36 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,9 +2,9 @@
 number= int(input())
 if (number>0):
    if(number%2=0):
-    print("even")
-elif(number!=0):
+     print("even")
+   elif(number!=0):
     print("odd")
-  else:
+else:
     print("invalid")
     
